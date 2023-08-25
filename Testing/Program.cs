e7064e34cedab7fcd4bd85fe0405161c2787c23e
Stanislav Slavov <stanislav.slavov88@gmail.com>
2023-08-25 19:14:06 +0300

    blablasda

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Reflection;
 
 namespace Testing
 {
@@ -14,10 +15,25 @@ namespace Testing
                 var endpoint = new Uri("https://www.wix.com/_serverless/hiring-task-spreadsheet-evaluator/sheets");
                 var result = client.GetAsync(endpoint).Result;
                 var json = result.Content.ReadAsStringAsync().Result;
-                Console.WriteLine(json);
+                //Console.WriteLine(json);
 
                 var excelData = JsonConvert.DeserializeObject<ExcelSheet>(json);
-                var test = excelData.Sheets[7];
+                var test = excelData.Sheets[7].Data;
+
+                Console.WriteLine(test);
+
+                //Type myType = test.GetType();
+                //IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
+
+                //foreach (PropertyInfo prop in props)
+                //{
+                //    object propValue = prop.GetValue(test, null);
+
+                //    // Do something with propValue
+
+                //    Console.WriteLine(propValue);
+
+                // }
             }
         }
     }
